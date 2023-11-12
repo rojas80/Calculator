99c5657d9207fb671739bfa4201ba2243f9cc9cf
hugo rojas <rojashugo80@gmail.com>
2023-11-12 02:19:05 -0800

    calculator all operations have been complete fully functional calculator

--- calculator.js
+++ calculator.js
@@ -1,14 +1,22 @@
 const  displayScreen = document.getElementById('cal-display');
 const buttons = document.querySelectorAll('.btn');
+let hasEqualBeenClick = false;
 let isCurrentNumberNegetive = false;
+let calculatorInput =  new Array();
 
 function buttonClickHandler(event) {
+    if(hasEqualBeenClick){
+        displayScreen.textContent = '';
+        hasEqualBeenClick = false;
+    }
+
    if((parseInt(event.target.value)>= 0 && parseInt(event.target.value) <= 9 ) || event.target.value =="."){
         updateDisplay(event.target.value);
    }
+
    switch(event.target.value){
     case 'ac':{
-        displayScreen.textContent = '';
+        clearAllData();
         break;
     }
     case 'del':{
@@ -23,7 +31,32 @@ function buttonClickHandler(event) {
         percentage();
         break;
     }
+    case '=':{ 
+        result();
+        break;
+    }
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+        //add operation and display value to the arraylist 
+        let  lastIndexValue = calculatorInput.length - 1;
+
+        if(displayScreen.textContent ==''){
+            if(calculatorInput[lastIndexValue] == '+' || calculatorInput[lastIndexValue ] == '-' ||
+                calculatorInput[lastIndexValue] == '*' || calculatorInput[lastIndexValue] == '/' ){
+                    calculatorInput[lastIndexValue] = event.target.value;
+            }
+        }
+        else{
+            calculatorInput.push(displayScreen.textContent);
+            calculatorInput.push(event.target.value);
+            displayScreen.textContent = '';
+        }
+        break;
    }
+   console.log(calculatorInput);
+   
    
 }
 
@@ -57,4 +90,70 @@ function percentage(){
     displayValue = displayValue / 100;
     displayScreen.textContent = displayValue.toString();
     
+}
+
+function clearAllData(){
+    displayScreen.textContent = '';
+    calculatorInput.length =  0;
+}
+
+function result(){
+    let total = 0;
+    let searchForOperates = ['*','/','+','-'];
+    let operatesIndex = 0;
+    let currentInputOperateIndex = 0;
+    let currentOperate = '';
+
+    hasEqualBeenClick  = true
+    calculatorInput.push(displayScreen.textContent);
+   
+    while(calculatorInput.length >1){
+        if(calculatorInput.indexOf(searchForOperates[operatesIndex]) == -1){
+            operatesIndex++;
+            continue;
+        }
+        currentInputOperateIndex  = calculatorInput.indexOf(searchForOperates[operatesIndex]);
+        currentOperate = searchForOperates[operatesIndex];
+
+        switch(currentOperate){
+            case '*':{
+                total  = multiply(calculatorInput[currentInputOperateIndex - 1], calculatorInput[currentInputOperateIndex + 1]);
+                break;
+            }
+            case '/':{
+                total  = divide(calculatorInput[currentInputOperateIndex - 1], calculatorInput[currentInputOperateIndex + 1]);
+                break;
+            }
+            case '+':{
+                total  = add(calculatorInput[currentInputOperateIndex - 1], calculatorInput[currentInputOperateIndex + 1]);
+                break;
+            }
+            case '-':{
+                total  = subtract(calculatorInput[currentInputOperateIndex - 1], calculatorInput[currentInputOperateIndex + 1]);
+                break;
+            }
+        }
+        calculatorInput.splice(currentInputOperateIndex);
+        calculatorInput.splice(currentInputOperateIndex + 1);
+        calculatorInput.splice(currentInputOperateIndex - 1);
+        calculatorInput.length = 0;
+    
+    }
+    displayScreen.textContent = total;
+}
+
+function add(firstNumber, secondNumber){    
+    return parseFloat(firstNumber) + parseFloat(secondNumber);
+
+}
+function subtract(firstNumber, secondNumber){
+    return parseFloat(firstNumber) - parseFloat(secondNumber);
+
+}
+function multiply(firstNumber,secondNumber){
+    return parseFloat(firstNumber) * parseFloat(secondNumber);
+
+}
+function divide(firstNumber,secondNumber){
+    return  parseFloat(firstNumber) / parseFloat(secondNumber);
 }
